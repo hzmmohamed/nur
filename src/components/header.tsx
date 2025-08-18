bcd30a693adbb9052dd408324c50c88c785cd2df
Hazem Fahmi <hzmmohamed@gmail.com>
2025-08-18 05:57:41 +0300

    UI Components + Updated header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="bg-accent text-foreground text-sm font-bold w-full h-[50px] flex justify-start items-center px-6">
+    <header className="bg-accent text-foreground text-sm font-bold w-full h-12 flex justify-start items-center px-6">
       Nur (Alpha)
     </header>
   );
