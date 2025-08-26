1e5233eba81f439c87ad64540ae0e25e666dfec8
Hazem Fahmi <hzmmohamed@gmail.com>
2025-08-26 21:13:55 +0300

    Refactor canvas to Konva + XState

--- src/lib/editor.machine.ts
+++ src/lib/editor.machine.ts
@@ -1,36 +1,203 @@
-import { createMachine } from "xstate";
+import {
+  assign,
+  createMachine,
+  fromCallback,
+  type ActorRefFromLogic,
+} from "xstate";
 import { frameFetcherMachine } from "./frame-fetcher.machine";
+import Konva from "konva";
+import ShortcutManager from "@keybindy/core";
 // import { timelineMachine } from "./timeline-machine";
 
+type Context = {
+  fps: number;
+  sceneId: string;
+  stageRef: Konva.Stage | null;
+  shortcutManager: ShortcutManager;
+  layers: Record<string, Konva.Layer>;
+};
 export const editorMachine = createMachine({
   types: {
     input: {} as { sceneId: string },
-    context: {} as { fps: number; sceneId: string },
+    context: {} as Context,
   },
   context: ({ input: { sceneId } }) => ({
     fps: 24,
     sceneId,
+    shortcutManager: new ShortcutManager(),
+    stageRef: null,
+    layers: {
+      currentFrame: new Konva.Layer({ id: "current-frame" }),
+      currentMask: new Konva.Layer({ id: "current-mask" }),
+    },
   }),
-  initial: "active",
+  initial: "loading",
+  invoke: [
+    {
+      src: frameFetcherMachine,
+      id: "frame-fetcher",
+      systemId: "frame-fetcher",
+      input: ({ context: { sceneId } }) => ({
+        sceneId,
+      }),
+    },
+    // { src: timelineMachine, id: "timeline", systemId: "timeline" },
+  ],
   states: {
+    loading: {
+      on: {
+        CONTAINER_MOUNTED: {
+          target: "active",
+          actions: [
+            assign({
+              stageRef: ({ event }) =>
+                new Konva.Stage({
+                  height: 2000,
+                  width: 2000,
+                  container: event.data.containerId,
+                }),
+            }),
+            ({ context }) => {
+              context.stageRef?.add(context.layers.currentFrame);
+              context.stageRef?.add(context.layers.currentFrame);
+            },
+          ],
+        },
+      },
+    },
     active: {
+      type: "parallel",
       invoke: [
         {
-          src: frameFetcherMachine,
-          id: "frame-fetcher",
-          systemId: "frame-fetcher",
-          input: ({ context: { sceneId } }) => ({
-            sceneId,
-          }),
+          input: ({ context }) => ({ layerRef: context.layers.currentFrame }),
+          id: "frame-renderer",
+          src: fromCallback<{ type: "" }, { layerRef: Konva.Layer }>(
+            ({ input: { layerRef }, system }) => {
+              (
+                system.get("frame-fetcher") as ActorRefFromLogic<
+                  typeof frameFetcherMachine
+                >
+              ).subscribe({
+                next({ context: { data } }) {
+                  if (data instanceof ImageBitmap) {
+                    layerRef.removeChildren();
+                    layerRef.add(
+                      new Konva.Image({
+                        x: 0,
+                        y: 0,
+                        image: data,
+                        width: data.width,
+                        height: data.height,
+                      })
+                    );
+                    layerRef.batchDraw();
+                  }
+                },
+              });
+            }
+          ),
         },
-        // { src: timelineMachine, id: "timeline", systemId: "timeline" },
+        {
+          input: ({ context }) => ({ ...context }),
+          id: "zoom-handler",
+          src: fromCallback<{ type: "" }, Context>(
+            ({ input: { stageRef } }) => {
+              const handleZoom: Konva.KonvaEventListener<
+                Konva.Stage,
+                WheelEvent
+              > = (event) => {
+                event.evt.preventDefault();
+                const oldScale = stageRef.scaleX();
+                const pointer = stageRef.getPointerPosition();
+                console.log(pointer);
+                if (pointer) {
+                  const zoomFactor = event.evt.deltaY > 0 ? 0.9 : 1.1;
+                  const newScale = Math.max(
+                    0.1,
+                    Math.min(oldScale * zoomFactor, 10)
+                  );
+                  const newPos = {
+                    x:
+                      pointer.x -
+                      ((pointer.x - stageRef.x()) * newScale) / oldScale,
+                    y:
+                      pointer.y -
+                      ((pointer.y - stageRef.y()) * newScale) / oldScale,
+                  };
+                  stageRef.scale({ x: newScale, y: newScale });
+                  stageRef.position({ x: newPos.x, y: newPos.y });
+                  // updateGrid();
+                  stageRef.batchDraw(); // Optimize by redrawing all layers at once
+                }
+              };
+              stageRef.on("wheel", handleZoom);
+              return () => {
+                // Clean up event listeners and destroy the stageRef
+                stageRef.off("wheel", handleZoom);
+              };
+            }
+          ),
+        },
       ],
+      states: {
+        mode: {
+          initial: "drawing",
+          invoke: {
+            input: ({ context }) => ({ ...context }),
+            id: "shortcut-handler",
+            src: fromCallback<
+              { type: "" },
+              { shortcutManager: ShortcutManager }
+            >(({ input: { shortcutManager }, sendBack }) => {
+              shortcutManager.register(
+                ["Space"],
+                (e) => {
+                  if (e.type === "keydown") {
+                    sendBack({ type: "PANNING_MODE_ENABLED" });
+                  } else {
+                    sendBack({ type: "PANNING_MODE_DISABLED" });
+                  }
+                },
+                { preventDefault: true, hold: true }
+              );
+              shortcutManager.start();
+
+              return () => shortcutManager.disableAll();
+            }),
+          },
+          states: {
+            drawing: {
+              on: {
+                PANNING_MODE_ENABLED: "panning",
+              },
+            },
+            panning: {
+              on: {
+                PANNING_MODE_DISABLED: "drawing",
+              },
+              entry: ({ context }) => {
+                if (context.stageRef) {
+                  console.log("panning");
+                  context.stageRef.draggable(true);
+                  context.stageRef.container().style.cursor = "grab";
+                }
+              },
+              exit: ({ context }) => {
+                if (context.stageRef) {
+                  context.stageRef.draggable(false);
+                  context.stageRef.container().style.cursor = "auto";
+                }
+              },
+            },
+          },
+        },
+      },
     },
   },
 });
 
 // type Context = {
-//   timelineActor: AnyActorRef;
+//   timelinector: AnyActorRef;
 //   frameFetcherActor: AnyActorRef;
 // };
 
--- src/routes/scenes/$id.tsx
+++ src/routes/scenes/$id.tsx
@@ -1,14 +1,15 @@
-import { Canvas } from "@/components/canvas";
 import { TimelinePanel } from "@/components/timeline-panel";
 import {
   ResizablePanelGroup,
   ResizablePanel,
   ResizableHandle,
 } from "@/components/ui/resizable";
-import { createActor, type ActorRefFromLogic } from "xstate";
 
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { editorMachine } from "@/lib/editor.machine";
+import { useActorRef } from "@xstate/react";
+import { useRef, useEffect } from "react";
+import { createActor, type ActorRefFromLogic } from "xstate";
 import type { frameFetcherMachine } from "@/lib/frame-fetcher.machine";
 // import type { timelineMachine } from "@/lib/timeline-machine";
 
@@ -23,6 +24,7 @@ export const Route = createFileRoute("/scenes/$id")({
     });
     editorActor.start();
     return {
+      editorActor,
       frameFetcher: editorActor.system.get(
         "frame-fetcher"
       ) as ActorRefFromLogic<typeof frameFetcherMachine>,
@@ -30,6 +32,9 @@ export const Route = createFileRoute("/scenes/$id")({
       //   typeof timelineMachine
       // >,
     };
+  },
+  onLeave: ({ context }) => {
+    context.editorActor.stop();
   },
   loader: async ({ params, context: { store } }) => {
     const name = store.getCell("scenes", params.id, "name");
@@ -39,6 +44,20 @@ export const Route = createFileRoute("/scenes/$id")({
 });
 
 function RouteComponent() {
+  const editorActorRef = Route.useRouteContext().editorActor;
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (containerRef.current) {
+      editorActorRef.send({
+        type: "CONTAINER_MOUNTED",
+        data: {
+          containerId: containerRef.current.id,
+        },
+      });
+    }
+  }, [editorActorRef, containerRef.current]);
+
   return (
     <ResizablePanelGroup direction="horizontal" className="h-full w-full">
       <ResizablePanel defaultSize={80}>
@@ -47,7 +66,21 @@ function RouteComponent() {
             defaultSize={75}
             className="flex items-center justify-center"
           >
-            <Canvas />
+            <div className="w-full h-full relative">
+              <div
+                ref={containerRef}
+                id="nur-canvas"
+                className="w-full h-full overflow-hidden"
+              >
+                {/* We no longer need this div because the Konva Stage is initialized with a direct container reference */}
+                {/* Display current scale and position for debugging/user feedback */}
+              </div>
+              {/* <div className="absolute top-4 left-4 bg-card text-card-foreground text-xs p-2 rounded-sm opacity-75">
+                <p>Scale: {stageScale.toFixed(2)}</p>
+                <p>X: {stageX.toFixed(2)}</p>
+                <p>Y: {stageY.toFixed(2)}</p>
+              </div> */}
+            </div>
           </ResizablePanel>
           <ResizableHandle withHandle />
           <TimelinePanel />
